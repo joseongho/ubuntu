964a880baac351c0c0498224f32a39689830d061
jo <chochoncnc@gmail.com>
2020-05-09 20:06:37 +0900

    first commit

---
+++ html/nav.js
@@ -0,0 +1,19 @@
+var i, tmp ;
+
+tmp = document.getElementsByTagName("a");
+
+for ( i = 0 ; i < tmp.length ; i++ ) {
+	tmp[i].style.display = "none";
+}
+
+document.getElementsByTagName("nav")[0].onmouseover = function () {  
+	for ( i = 0 ; i < tmp.length ; i++ ) {
+		tmp[i].style.display = "block";
+	} ;
+}
+
+document.getElementsByTagName("nav")[0].onmouseout= function () {  
+	for ( i = 0 ; i < tmp.length ; i++ ) {
+		tmp[i].style.display = "none";
+	} ;
+}
