e883e93bcafc07854eab77849e1eb808b529fe48
jo <chochoncnc@gmail.com>
2020-05-11 16:56:40 +0900

    drag and drop

---
+++ javascriptDrag/test.js
@@ -0,0 +1,14 @@
+function drag(ev) {
+	ev.dataTransfer.setData("text",ev.target.id);
+}
+
+function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(data));
+}
+
+function allowDrop(ev) {
+	ev.preventDefault();
+}
+	
